d233df36d9fbebc6dea06c99d805a9298450e7d2
pauladarias <pauladarias@gmail.com>
2020-05-06 17:35:43 +0100

    added in bookmark object, 4 failing tests

--- spec/features/create_bookmark_spec.rb
+++ spec/features/create_bookmark_spec.rb
@@ -5,8 +5,9 @@ feature 'Create bookmark' do
   scenario 'A user can add a bookmark' do
     visit('/bookmarks/new')
     fill_in 'url', with: 'http://www.youtube.com'
+    fill_in 'title', with: 'Youtube'
     click_button('Submit')
 
-    expect(page).to have_content("http://www.youtube.com")
+    expect(page).to have_link('Youtube', href: 'http://www.youtube.com')
   end
 end
